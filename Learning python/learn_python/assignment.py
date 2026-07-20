383d5f655313d064e985514bd647470dad832e1d
munal <emailaddress9810@gmail.com>
2026-07-20 21:52:27 +0545

    Learning the Basics of Data cleaning using pandas

---
+++ Learning python/learn_python/assignment.py
@@ -0,0 +1,58 @@
+
+# print("Welcome to DPLMS student registration system")
+
+# courses = ["Python with AI/ML", "JavaScript", "Flutter", "MERN Stack"]
+
+# for i in range(len(courses)):
+#     print(courses[i])
+    
+# student_name = input("Enter your name : ")
+# email = input("Enter your email : ")
+# age = input("Enter your age : ")
+# selected_course = input("Enter your selected course : ")
+
+# student_details = {
+#     "Name" : student_name,
+#     "Email" : email,
+#     "Age" : age,
+#     "Selected Course" : selected_course
+# }
+
+# if selected_course in courses:
+#     print("Registration Successful")
+# else :
+#     print("Course not available")
+    
+# print("-----Student Details-----")
+# for key, value in student_details.items():
+#     print(f"{key} : {value}")
+
+
+#---------------------------------------------While loop -------------------------------
+
+# sum = 0
+# while sum <10 :
+#     sum = sum +1 ;
+#     print(sum)
+
+# password = ""
+# while password != "secret12":
+#     password = input("Enter password: ")
+
+# print("Login successful!")
+
+# lives = 3
+
+# while lives > 0:
+#     print("Playing...")
+#     lives = lives - 1
+
+# print("Game Over")
+
+download_complete = False
+
+while not download_complete:
+    print("Downloading...")
+    download_complete = True
+
+print("Download complete!")
---
+++ Learning python/learn_python/day5.py
@@ -0,0 +1,19 @@
+
+# username = "admin"
+# password = "1234"
+
+# Username = input("Enter Username : ")
+# Password = input("Enter password : ")
+
+# if (Username==username and Password==password) :
+#     print("Login succesfull")
+
+# # elif ->  elseif in python
+    
+# else :
+#     print("Login Unsuccessfull")
+    
+# ---------------------------For Loop--------------------------------------------------    
+
+for i in range(10,0,-1) :
+    print(i)
